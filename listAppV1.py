b3bfeca998e4bca7b4ce54a6bdb8b959d1e569ad
zerochaos0-0 <79869240+zerochaos0-0@users.noreply.github.com>
2021-03-11 12:20:09 -0700

    Add files via upload

--- listAppV1.py
+++ listAppV1.py
@@ -8,22 +8,42 @@ Program Goals:
 """
 
 #create functions that will perform the above actions
+import random
+myList = []
+
 
 def mainProgram():
-    myList = []
-    print("Hello, there! Let's work with lists!")
-    print("Choose one of the following opitons. Type a number ONLY!")
-    choice = input("1. Add to list, 2. Return the value at an index position   ")
-    if choice == "1":
-        addToList[]
-    elif choice == "2":
-        indexValues
+    while True:
+        print("Hello, there! Let's work with lists!")
+        print("Choose one of the following opitons. Type a number ONLY!")
+        choice = input("""1. Add to list,
+2. Return the value at an index position,
+3. Generate a random number from the list
+4. End the program   """)
+        if choice == "1":
+            addToList()
+        elif choice == "2":
+            indexValues()
+        elif choice == "4":
+            break
+        elif choice == "3":
+            randomSearch()
     #we need to add an exit program as well as error catching
 
         
 def addToList():
+    print("Okay, you want to add to your list!")
+    newItem = input("What interger would you like to add?  ")
+    myList.append(int(newItem))
+    print(myList)
 
 def indexValues():
+    indexPos = input("Which index position would you like to know?  ")
+    print(myList[int(indexPos)])
+
+def randomSearch():
+    print("Here's a random value from your list!")
+    print(myList[random.randint(0, len(myList)-1)])
 
 if __name__ == "__main__":
     mainProgram()
